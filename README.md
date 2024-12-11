7fee8a6242b0557360a007e9d9ffcbdae9a420fd
silentic <tangadaniel160@gmail.com>
2024-12-11 23:11:35 +0200

    Update README.md

--- README.md
+++ README.md
@@ -10,6 +10,9 @@ git clone "https://github.com/GabrielVrabie007/Project_Sigmoid_AI.git"
 ## Install dependencies
 pip install -r requirements.txt
 
+## Runing the app
+streamlit run app/app.py
+
 ### Link for card fraud detection dataset
 
 https://www.kaggle.com/datasets/kartik2112/fraud-detection
--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ import pandas as pd
 with st.sidebar:
     col1, col2 = st.columns(2, vertical_alignment="center")
     with col1:
-        st.image("logo.png", width=100)
+        st.image("app/logo.png", width=100)
     with col2:
         st.markdown(
             "<h1 style='font-size:40px; margin:0; margin-bottom:10px;'>Finance</h1>",
